97bc2dbb0541879a624909759c5ea95c362ad377
koji-torishima <torikou3685@gmail.com>
2020-09-24 04:41:34 +0900

    調査終了

--- PickerForOS14/ViewController.swift
+++ PickerForOS14/ViewController.swift
@@ -10,13 +10,12 @@
 // TextFieldを使って日付を選択する時に使うやり方。
 
 import UIKit
-import SwiftUI
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var textField: UITextField!
-    @IBOutlet weak var dateLabel: UILabel!
+//    @IBOutlet weak var dateLabel: UILabel!
     
     private var textFieldInputDatePicker = UIDatePicker()
     
@@ -26,43 +25,36 @@ class ViewController: UIViewController {
     }
     
     private func setup() {
-        datePicker.preferredDatePickerStyle = .compact
-//        datePicker.datePickerMode = .countDownTimer
+//        datePicker.preferredDatePickerStyle = .wheels
+//        datePicker.datePickerMode = .time
 //        datePicker.datePickerMode = .date
 //        datePicker.datePickerMode = .dateAndTime
 //        datePicker.datePickerMode = .time
 //        datePicker.preferredDatePickerStyle = .compact
-        textField.inputView = textFieldInputDatePicker
+//        textField.inputView = textFieldInputDatePicker
         /// これはTextField選択時のモードを選択できる。
-//        textFieldInputDatePicker.preferredDatePickerStyle = .compact
+        textFieldInputDatePicker.preferredDatePickerStyle = .wheels
         
-//        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 10))
-//        let spacelItem = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
-//        let doneItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
-//        toolbar.setItems([spacelItem, doneItem], animated: true)
+       let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 40))
+       let spacelItem = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        let doneItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        toolbar.setItems([spacelItem, doneItem], animated: true)
 
         // インプットビュー設定(紐づいているUITextfieldへ代入)
         textField.inputView = textFieldInputDatePicker
-//        textField.inputAccessoryView = toolbar
+        textField.inputAccessoryView = toolbar
     }
     
     @IBAction func dateChanged(_ sender: UIDatePicker) {
-        dateLabel.text = "\(datePicker.date)"
+//        dateLabel.text = "\(datePicker.date)"
         
     }
     
     
     @objc func done() {
-           textField.endEditing(true)
-           // 日付のフォーマット
-           let formatter = DateFormatter()
-           //"yyyy年MM月dd日"を"yyyy/MM/dd"したりして出力の仕方を好きに変更できるよ
-           formatter.dateFormat = "yyyy年MM月dd日"
-           //(from: datePicker.date))を指定してあげることで
-           //datePickerで指定した日付が表示される
-           textField.text = "\(formatter.string(from: textFieldInputDatePicker.date))"
-       }
-
-
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy年MM月dd日"
+        textField.text = "\(formatter.string(from: textFieldInputDatePicker.date))"
+    }
 }
 
